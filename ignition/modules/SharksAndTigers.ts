3fe9c967eee23e646478178cf9aa134201afeb01
am-hernandez <angel@mouseandweb.com>
2025-12-12 05:09:53 -0500

    chore: remove deprecated files

--- ignition/modules/SharksAndTigers.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("SharksAndTigersFactoryModule", (m) => {  
-  const sharksAndTigersFactory = m.contract("SharksAndTigersFactory");
-
-  return { sharksAndTigersFactory };
-});
