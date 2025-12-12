7ce9c02d23561f3721cbc0d8e29969fb8ca95c51
am-hernandez <angel@mouseandweb.com>
2025-12-12 05:28:00 -0500

    chore: fix remappings

--- test/SharksAndTigersGame.t.sol
+++ test/SharksAndTigersGame.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {SharksAndTigersFactory} from "src/SharksAndTigersFactory.sol";
 import {SharksAndTigers} from "src/SharksAndTigers.sol";
 
