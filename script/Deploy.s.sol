aabac00a7dbc157b271c84cf03570265a2c7ce9c
am-hernandez <angel@mouseandweb.com>
2025-12-14 23:36:42 -0500

    chore: add deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Script.sol";
+import {SharksAndTigersFactory} from "../src/SharksAndTigersFactory.sol";
+import "@openzeppelin/mocks/token/ERC20Mock.sol";
+
+contract Deploy is Script {
+    function run() external {
+        address usdcToken;
+
+        vm.startBroadcast();
+
+        // On local chains (Anvil), deploy a mock USDC token
+        // On testnets/mainnets, use USDC_TOKEN from environment
+        if (block.chainid == 31337) {
+            // Local Anvil chain - deploy mock USDC
+            ERC20Mock mockUsdc = new ERC20Mock();
+            usdcToken = address(mockUsdc);
+        } else {
+            // Testnet/Mainnet - use real USDC from environment
+            usdcToken = vm.envAddress("USDC_TOKEN");
+            console.log("Using USDC token from environment:", usdcToken);
+        }
+
+        SharksAndTigersFactory factory = new SharksAndTigersFactory(usdcToken);
+
+        vm.stopBroadcast();
+
+        console.log("Deployer address:", msg.sender);
+        console.log("SharksAndTigersFactory deployed at:", address(factory));
+        console.log("USDC Token address:", usdcToken);
+        console.log("Chain ID:", block.chainid);
+    }
+}
+
